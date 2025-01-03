4ec42fdfbaf16ab2fdbfe4f44d960273b15f784c
yfdwl <37701729+yfdwl@users.noreply.github.com>
2025-01-03 11:52:33 +0200

    Add support for exporting data to Google Sheets

--- start.js
+++ start.js
@@ -4,7 +4,7 @@ var mkdirp  = require('mkdirp')
 var path    = require('path')
 var answers = path.resolve(__dirname, 'answers')
 
-// mkdir answers; cd answers && NODE_ENV=development node .. r9zx44lvAk
+// mkdir answers; cd answers && NODE_ENV=development node ..
 mkdirp.sync(answers)
 process.chdir(answers)
 require('./')
