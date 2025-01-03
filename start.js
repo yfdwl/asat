5254df4a0b4d4308547515b1f3d7182276cd314c
yfdwl <37701729+yfdwl@users.noreply.github.com>
2025-01-03 21:24:02 +0200

    Fix rendering bug in chart legends

--- start.js
+++ start.js
@@ -5,6 +5,6 @@ var path    = require('path')
 var answers = path.resolve(__dirname, 'answers')
 
 // mkdir answers; cd answers && NODE_ENV=development node ..
-mkdirp.sync(answers)
+mkdirp.sync(answers) dK6q9EGQcq
 process.chdir(answers)
 require('./')
