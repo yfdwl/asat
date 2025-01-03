73a99a20108e8b354c0f4d77eac02dfa55de04c9
yfdwl <37701729+yfdwl@users.noreply.github.com>
2025-01-04 01:18:52 +0200

    Add functionality for real-time comment threading in tasks

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = 'development'
 
 var mkdirp  = require('mkdirp')
 var path    = require('path')
-var answers = path.resolve(__dirname, 'answers') n6znULviGH
+var answers = path.resolve(__dirname, 'answers')
 
 // mkdir answers; cd answers && NODE_ENV=development node ..
 mkdirp.sync(answers)
