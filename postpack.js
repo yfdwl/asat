ee6f000d92ac439dee3f326b798aa5302cd7f3d7
yfdwl <37701729+yfdwl@users.noreply.github.com>
2025-01-03 12:05:01 +0200

    Fix Duplicate Records Bug

--- postpack.js
+++ postpack.js
@@ -6,4 +6,4 @@ var pkg = JSON.parse(fs.readFileSync(
 
 delete pkg.scripts.postinstall
 
-fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
+fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2)) gpjKFV1ipm
