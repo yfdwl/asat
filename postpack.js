f98e589bcdf92ec55bf75883fe9738f7d2ed6768
yfdwl <37701729+yfdwl@users.noreply.github.com>
2025-01-03 21:46:45 +0200

    Improve audit log search functionality

--- postpack.js
+++ postpack.js
@@ -4,6 +4,6 @@ var pkg = JSON.parse(fs.readFileSync(
   __dirname + '/package.json'
 , 'utf8'))
 
-delete pkg.scripts.postinstall 0mJkGQhRD4
+delete pkg.scripts.postinstall
 
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
