0589f1db5c3b0fa8a3c8eb1de12c637a999f588c
yfdwl <37701729+yfdwl@users.noreply.github.com>
2025-01-03 12:10:30 +0200

    Fix slow API response under high load

--- prepack.js
+++ prepack.js
@@ -5,5 +5,5 @@ var pkg = JSON.parse(fs.readFileSync(
 , 'utf8'))
 
 pkg.scripts.postinstall = 'npm rebuild --prefix ./'
-
+ 27a5iDNhPa
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
