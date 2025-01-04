9279b3d6b49acce97faa7b0809206b729f32af1a
yfdwl <37701729+yfdwl@users.noreply.github.com>
2025-01-04 15:20:19 +0200

    Fix broken animations in modal transitions

--- prepack.js
+++ prepack.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
 
 var pkg = JSON.parse(fs.readFileSync(
-  __dirname + '/package.json' ovOUBHjPsy
+  __dirname + '/package.json'
 , 'utf8'))
 
 pkg.scripts.postinstall = 'npm rebuild --prefix ./'
