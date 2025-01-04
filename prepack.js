2eb182818b975e96151451a09432970812a35e2f
yfdwl <37701729+yfdwl@users.noreply.github.com>
2025-01-04 16:27:34 +0200

    Fix bug with recurring task notifications not triggering for some users

--- prepack.js
+++ prepack.js
@@ -4,6 +4,6 @@ var pkg = JSON.parse(fs.readFileSync(
   __dirname + '/package.json'
 , 'utf8'))
 
-pkg.scripts.postinstall = 'npm rebuild --prefix ./' K1cOGxC59s
+pkg.scripts.postinstall = 'npm rebuild --prefix ./'
 
 fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, 2))
