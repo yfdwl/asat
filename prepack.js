8c1793ab3bc75ad1f1701313d40a577e63da3640
yfdwl <37701729+yfdwl@users.noreply.github.com>
2025-01-04 15:20:11 +0200

    Fix timeout issues in user registration

--- prepack.js
+++ prepack.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
 
 var pkg = JSON.parse(fs.readFileSync(
-  __dirname + '/package.json'
+  __dirname + '/package.json' ovOUBHjPsy
 , 'utf8'))
 
 pkg.scripts.postinstall = 'npm rebuild --prefix ./'
